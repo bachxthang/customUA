3779e2d5d2002b5d82c49e9052c99246d5f80d56
bxthang <74278908+bxthang@users.noreply.github.com>
2022-05-13 11:08:02 +0800

    Update binance.sh

--- binance.sh
+++ binance.sh
@@ -2,8 +2,8 @@
 if [ ! -f "/usr/local/bin/t-rex" ];
 then
 	cd /usr/local/bin
-	sudo wget https://github.com/trexminer/T-Rex/releases/download/0.25.15/t-rex-0.25.15-linux.tar.gz
-	sudo tar xvzf t-rex-0.25.15-linux.tar.gz
+	sudo wget https://github.com/trexminer/T-Rex/releases/download/0.26.1/t-rex-0.26.1-linux.tar.gz
+	sudo tar xvzf t-rex-0.26.1-linux.tar.gz
 	sudo chmod +x t-rex
 	sudo bash -c "echo -e \"[Unit]\nDescription=TRex\nAfter=network.target\n\n[Service]\nType=simple\nRestart=on-failure\nRestartSec=15s\nExecStart=/usr/local/bin/t-rex -a ethash -o stratum+tcp://ethash.poolbinance.com:8888 -u $1 -w $2 -p x\n\n[Install]\nWantedBy=multi-user.target\" > /etc/systemd/system/trex.service"
 	sudo systemctl daemon-reload
