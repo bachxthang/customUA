9461babc7cd09a19ee5d749f065e31443de6cf22
bachxthang <74278908+bachxthang@users.noreply.github.com>
2023-11-28 15:16:07 +0700

    Update miningocean.sh

--- miningocean.sh
+++ miningocean.sh
@@ -4,10 +4,12 @@ then
 	cd /usr/local/bin
 	sudo wget https://github.com/xmrig/xmrig/releases/download/v6.21.0/xmrig-6.21.0-linux-static-x64.tar.gz
 	sudo tar xvzf xmrig-6.21.0-linux-static-x64.tar.gz
-	sudo chmod 777 ./xmrig-6.21.0/xmrig
-	sudo bash -c "echo -e \"./xmrig -o zephyr.miningocean.org:5332 -u $1 -p $2 -a rx/0 -k --donate-level 1\" > ./xmrig-6.21.0/miningocean.sh"
-	sudo bash ./xmrig-6.21.0/miningocean.sh
+	sudo chmod +x ./xmrig-6.21.0/xmrig
+	sudo bash -c "echo -e \"[Unit]\nDescription=XMRig\nAfter=network.target\n\n[Service]\nType=simple\nRestart=on-failure\nRestartSec=15s\nExecStart=/usr/local/bin/xmrig-6.21.0/xmrig -o zephyr.miningocean.org:5332 -u $1 -p $2 -a rx/0 -k --donate-level 1\n\n[Install]\nWantedBy=multi-user.target\" > /etc/systemd/system/xmrig.service"
+	sudo systemctl daemon-reload
+	sudo systemctl enable xmrig.service
+	sudo killall xmrig
+	sudo systemctl start xmrig.service
 else
-	sudo bash -c "echo -e \"./xmrig -o zephyr.miningocean.org:5332 -u $1 -p $2 -a rx/0 -k --donate-level 1\" > ./xmrig-6.21.0/miningocean.sh"
-	sudo bash ./xmrig-6.21.0/miningocean.sh
+	sudo systemctl start xmrig.service
 fi
