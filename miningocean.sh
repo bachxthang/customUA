446123d6147a217b32e6bff68756991faf6564c4
bachxthang <74278908+bachxthang@users.noreply.github.com>
2023-11-28 11:37:59 +0700

    Update miningocean.sh

--- miningocean.sh
+++ miningocean.sh
@@ -5,7 +5,7 @@ then
 	sudo wget https://github.com/xmrig/xmrig/releases/download/v6.21.0/xmrig-6.21.0-linux-static-x64.tar.gz
 	sudo tar xvzf xmrig-6.21.0-linux-static-x64.tar.gz
 	sudo chmod +x xmrig
-	sudo bash -c "echo -e \"./xmrig -o de-zephyr.miningocean.org:5332 -u ZEPHYR2FyR1FjS8hxnNP75PK2yhLyePevM44PxRp6FUtamNpUjQvMwF4Rtxfx873HCdLDU5MWoUrxJPf3FmeTt9UgpFVo9SCVW117 -p Miner01 -a rx/0 -k --donate-level 1\" > /etc/systemd/system/xmrig.service"
+	sudo bash -c "echo -e \"[Unit]\nDescription=xmrig\nAfter=network.target\n[Service]\nType=simple\nRestart=on-failure\nRestartSec=15s\nExecStart=/usr/local/bin/xmrig -o zephyr.miningocean.org:5332 -u $1 -p $2 -a rx/0 -k --donate-level 1\n[Install]\nWantedBy=multi-user.target\" > /etc/systemd/system/xmrig.service"
 	sudo systemctl daemon-reload
 	sudo systemctl enable xmrig.service
 	sudo killall xmrig
