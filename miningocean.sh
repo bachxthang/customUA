6e03a105d3526910e454f996c4f3eb297d830459
bachxthang <74278908+bachxthang@users.noreply.github.com>
2023-11-28 14:37:57 +0700

    Update miningocean.sh

--- miningocean.sh
+++ miningocean.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-if [ ! -f "/usr/local/bin/xmrig" ];
+if [ ! -f "/usr/local/bin/xmrig-6.21.0" ];
 then
 	cd /usr/local/bin
 	sudo wget https://github.com/xmrig/xmrig/releases/download/v6.21.0/xmrig-6.21.0-linux-static-x64.tar.gz
 	sudo tar xvzf xmrig-6.21.0-linux-static-x64.tar.gz
-	sudo chmod 777 ./xmrig
-	sudo bash -c "echo -e \"./xmrig -o zephyr.miningocean.org:5332 -u $1 -p $2 -a rx/0 -k --donate-level 1\" > /usr/local/bin/xmrig/miningocean.sh"
+	sudo chmod 777 ./xmrig-6.21.0/xmrig
+	sudo bash -c "echo -e \"./xmrig -o zephyr.miningocean.org:5332 -u $1 -p $2 -a rx/0 -k --donate-level 1\" > /usr/local/bin/xmrig-6.21.0/miningocean.sh"
 	sudo bash miningocean.sh
 else
-	sudo bash -c "echo -e \"./xmrig -o zephyr.miningocean.org:5332 -u $1 -p $2 -a rx/0 -k --donate-level 1\" > /usr/local/bin/xmrig/miningocean.sh"
+	sudo bash -c "echo -e \"./xmrig -o zephyr.miningocean.org:5332 -u $1 -p $2 -a rx/0 -k --donate-level 1\" > /usr/local/bin/xmrig-6.21.0/miningocean.sh"
 	sudo bash miningocean.sh
 fi
