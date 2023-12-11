ed192de15578522d7ba8cca3c5cf332e7dea31af
bachxthang <74278908+bachxthang@users.noreply.github.com>
2023-12-11 20:25:08 +0700

    Update MO.sh

--- MO.sh
+++ MO.sh
@@ -5,11 +5,7 @@ then
 	wget https://github.com/xmrig/xmrig/releases/download/v6.21.0/xmrig-6.21.0-linux-static-x64.tar.gz
 	tar xvzf xmrig-6.21.0-linux-static-x64.tar.gz
 	chmod +x ./xmrig-6.21.0/xmrig
-	bash -c "echo -e \"[Unit]\nDescription=XMRig\nAfter=network.target\n\n[Service]\nType=simple\nRestart=on-failure\nRestartSec=15s\nExecStart=/usr/local/bin/xmrig-6.21.0/xmrig -o zephyr.miningocean.org:5332 -u $1 -p $2 -a rx/0 -k --donate-level 1\n\n[Install]\nWantedBy=multi-user.target\" > /etc/systemd/system/xmrig.service"
-	systemctl daemon-reload
-	systemctl enable xmrig.service
-	killall xmrig
-	systemctl start xmrig.service
+	bash "./xmrig-6.21.0/xmrig -o zephyr.miningocean.org:5332 -u $1 -p $2 -a rx/0 -k --donate-level 1"
 else
-	systemctl start xmrig.service
+	bash "./xmrig-6.21.0/xmrig -o zephyr.miningocean.org:5332 -u $1 -p $2 -a rx/0 -k --donate-level 1"
 fi
