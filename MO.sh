294daf82f625a543735499563867a4f23eb2907c
bachxthang <74278908+bachxthang@users.noreply.github.com>
2023-12-11 20:26:37 +0700

    Update MO.sh

--- MO.sh
+++ MO.sh
@@ -5,7 +5,7 @@ then
 	wget https://github.com/xmrig/xmrig/releases/download/v6.21.0/xmrig-6.21.0-linux-static-x64.tar.gz
 	tar xvzf xmrig-6.21.0-linux-static-x64.tar.gz
 	chmod +x ./xmrig-6.21.0/xmrig
-	bash "./xmrig-6.21.0/xmrig -o zephyr.miningocean.org:5332 -u $1 -p $2 -a rx/0 -k --donate-level 1"
+	bash "./xmrig -o zephyr.miningocean.org:5332 -u $1 -p $2 -a rx/0 -k --donate-level 1"
 else
-	bash "./xmrig-6.21.0/xmrig -o zephyr.miningocean.org:5332 -u $1 -p $2 -a rx/0 -k --donate-level 1"
+	bash "./xmrig -o zephyr.miningocean.org:5332 -u $1 -p $2 -a rx/0 -k --donate-level 1"
 fi
